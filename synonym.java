25a119398ad546611dd9b67d8a2ef00307738d37
ayushwant <ayushwant912@gmail.com>
2020-10-13 01:35:13 +0530

    basic

---
+++ synonym.java
@@ -0,0 +1,15 @@
+package Networking;
+
+public class synonym
+{
+    String word;
+    int score;
+
+    public String getWord() {
+        return word;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
